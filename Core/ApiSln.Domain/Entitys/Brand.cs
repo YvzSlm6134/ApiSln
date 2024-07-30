5338be05f826b8a3ec41aac0dd39f20da72a74dc
Yavuz Selim Bayraktar <yavuzselimbayraktar61@gmail.com>
2024-07-30 15:23:29 +0300

    Entitiyler oluşturuldu

---
+++ Core/ApiSln.Domain/Entitys/Brand.cs
@@ -0,0 +1,23 @@
+﻿using ApiSln.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSln.Domain.Entitys
+{
+    public class Brand : EntityBase
+    { 
+    public Brand() 
+    {
+
+    }
+    public Brand(string name) 
+    {
+            Name = name;
+        }
+     
+        public required string Name { get; set; }
+    }
+}
