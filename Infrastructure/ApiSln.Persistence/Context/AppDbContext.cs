3343e7b7c2de0255ca3ede88b3c659f3aa2a0880
Yavuz Selim Bayraktar <yavuzselimbayraktar61@gmail.com>
2024-08-06 13:36:27 +0300

    Identity Db' ye geçildi

--- Infrastructure/ApiSln.Persistence/Context/AppDbContext.cs
+++ Infrastructure/ApiSln.Persistence/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ApiSln.Domain.Entitys;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,7 @@ using YoutubeApi.Domain.Entities;
 
 namespace ApiSln.Persistence.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<User,Role,Guid>
     {
          public AppDbContext() { }
 
--- Infrastructure/ApiSln.Persistence/Registration.cs
+++ Infrastructure/ApiSln.Persistence/Registration.cs
@@ -1,5 +1,6 @@
 ﻿using ApiSln.Application.İnterface.Repositories;
 using ApiSln.Application.İnterface.UnitOfWorks;
+using ApiSln.Domain.Entitys;
 using ApiSln.Persistence.Context;
 using ApiSln.Persistence.Repositories;
 using ApiSln.Persistence.UnitOfWorks;
@@ -26,6 +27,18 @@ namespace ApiSln.Persistence
 
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddIdentityCore<User>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 2;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireDigit = false;
+                opt.SignIn.RequireConfirmedEmail = false;
+            })
+                .AddRoles<Role>()
+                .AddEntityFrameworkStores<AppDbContext>();
         }
 
     }
