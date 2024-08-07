71b5cb17f9d89e0145034a3f04e4c4e008d0a853
Yavuz Selim Bayraktar <yavuzselimbayraktar61@gmail.com>
2024-08-07 14:59:53 +0300

    token service tamamlandı ve swagger'da auth arayüzü tamamlandı.

--- Presentation/ApiSln.Api/Program.cs
+++ Presentation/ApiSln.Api/Program.cs
@@ -3,6 +3,7 @@ using ApiSln.Application;
 using ApiSln.Infrastructure;
 using ApiSln.Mapper;
 using ApiSln.Application.Exceptions;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +26,34 @@ builder.Services.AddPersistance(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddCustomMapper();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Solution ", Version = "v1", Description = "API Solution swagger client." });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization ",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "'Bearer yazýp boþluk býraktýktan sonra Token'ý Girebilirsiniz \r\n\r\n Örnreðin : \"Bearer  "
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()  
+            }
+    });
+});
 
 var app = builder.Build();
 
